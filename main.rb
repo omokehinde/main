ea03088839c8b9f80db4a658999830eadc258196
omokehinde I <engr.omokehinde@gmail.com>
2016-02-11 08:59:44 +0100

    Importing all the code

---
+++ main.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'erb'
+require 'tilt/erubis'
+require "./song"
+
+get '/'do
+	erb :home
+end
+
+get '/about'  do
+	@title = "All About This Website"
+	erb :about
+end
+
+get '/contact' do
+	@title = "Omokehinde"
+	erb :contact
+end
+
+not_found do
+	erb :not_found
+end
---
+++ song.rb
@@ -0,0 +1,41 @@
+require "dm-core"
+require "dm-migrations"
+require "erb"
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+
+class Song
+	include DataMapper::Resource
+	property :id, Serial
+	property :title, String
+	property :lyrics, Text
+	property :length, Integer
+	property :released_on, Date
+
+	def released_on = date
+		super Date.strptime(date, '%m/%d/%Y')
+	end
+
+end
+
+DataMapper.finalize
+
+get '/songs' do
+	@songs = Song.all
+	erb :songs
+end
+
+get '/songs/new' do
+	@song = Song.new
+	erb :new_song
+end
+
+get '/songs/:id' do
+	@song = Song.get(params[:id])
+	erb :show_song
+end
+
+post '/songs' do
+	song = Song.create(params[:song])
+	redirect to('/songs/#{song.id}')
+end
